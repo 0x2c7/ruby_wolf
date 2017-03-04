d7d653f0b37477aaf8e5717737168f23ff66496f
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2017-03-05 00:14:23 +0700

    Fix headers missing problem

--- lib/ruby_wolf/handler.rb
+++ lib/ruby_wolf/handler.rb
@@ -2,28 +2,38 @@ require 'http/parser'
 
 module RubyWolf
   class Handler
-
-    attr_reader :app, :env, :connection, :response, :callback, :body, :headers, :status
+    attr_reader :app, :env, :connection, :response
 
     def initialize(app, connection, &callback)
       @app = app
-      @connection = connection
-      @callback = callback
       @env = {}
+      @connection = connection
       @response = ''
+
+      @callback = callback
     end
 
     def process
       prepare_rack_env
-      generate_response
-      callback.call(response) if callback
-      response
+      log_request
+
+      @status, @headers, @body = @app.call(env)
+
+      compose_response
+      log_response
+
+      @callback.call(response) if @callback
+      @response
     end
 
     private
 
     def prepare_rack_env
-      @env = ENV.to_h.merge(
+      @connection.headers.each do |key, value|
+        @env[rack_key(key)] = value
+      end
+
+      @env = @env.merge(
         'rack.version' => ::Rack::VERSION,
         'rack.errors' => STDERR,
         'rack.multithread'  => false,
@@ -31,69 +41,53 @@ module RubyWolf
         'rack.runonce'      => true,
         'rack.url_scheme'   => ENV['HTTPS'] ? 'https' : 'http',
 
-        'REQUEST_METHOD' => connection.method,
-        'REQUEST_PATH' => connection.path,
-        'PATH_INFO' => connection.path,
-        'QUERY_STRING' => connection.query,
+        'REQUEST_METHOD' => @connection.method,
+        'REQUEST_PATH' => @connection.path,
+        'PATH_INFO' => @connection.path,
+        'QUERY_STRING' => @connection.query,
 
         'SERVER_PROTOCOL' => 'HTTP/1.1',
         'SERVER_NAME' => 'Ruby Wolf',
         'HTTP_VERSION' => 'HTTP/1.1',
-        'HTTP_HOST' => connection.headers['Host'],
-        'HTTP_USER_AGENT' => connection.headers['User-Agent'],
-        'HTTP_ACCEPT' => connection.headers['Accept'],
-        'CONTENT_LENGTH' => connection.headers['Content-Length'],
-        'CONTENT_TYPE' => connection.headers['Content-Type'],
 
-        'rack.input' => StringIO.new(connection.read_chunk)
+        'rack.input' => StringIO.new(@connection.read_chunk)
       )
-      log_request
-    end
-
-    def generate_response
-      @status, @headers, @body = app.call(env)
-      compose_response
-      log_response
     end
 
     def compose_response
-      @response += "HTTP/1.1 #{status} #{RubyWolf::CRLF}"
-      headers.each do |key, value|
+      @response += "HTTP/1.1 #{@status} #{RubyWolf::CRLF}"
+      @headers.each do |key, value|
         @response += "#{key}: #{value}#{RubyWolf::CRLF}"
       end
 
       @response += RubyWolf::CRLF
-      body.each do |part|
+      @body.each do |part|
         @response += part
       end
     ensure
-      body.close if body.respond_to? :close
+      @body.close if @body.respond_to? :close
     end
 
     private
 
     def log_request
-      RubyWolf.logger.info(
-        [
-          'HTTP/1.1',
-          connection.method,
-          request_path
-        ].join(' ')
-      )
+      RubyWolf.logger.info("HTTP/1.1 #{@connection.method} #{request_path}")
     end
 
     def log_response
-      RubyWolf.logger.info(
-        "Response HTTP/1.1 #{status}"
-      )
+      RubyWolf.logger.info("Response HTTP/1.1 #{@status}")
     end
 
     def request_path
-      if connection.query
-        "#{connection.path}?#{connection.query}"
+      if @connection.query
+        "#{@connection.path}?#{@connection.query}"
       else
-        connection.path
+        @connection.path
       end
+    end
+
+    def rack_key(key)
+      "HTTP_#{key.upcase.gsub(/[^0-9A-Z]/, '_')}"
     end
   end
 end
