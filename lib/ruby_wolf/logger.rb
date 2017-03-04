5f83bc9ab63125844883566bba49945e949a037c
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2017-03-05 00:14:33 +0700

    Change Logger format

--- lib/ruby_wolf/logger.rb
+++ lib/ruby_wolf/logger.rb
@@ -2,31 +2,39 @@ require 'logger'
 
 module RubyWolf
   class Logger < ::Logger
-    def info(contents = "")
+    def initialize(*args)
+      super(*args)
+      @formatter = proc do |severity, datetime, _progname, msg|
+        date_format = datetime.strftime('%Y-%m-%d %H:%M:%S')
+        "[#{severity.to_s[0]}] [#{date_format}] #{msg}\n"
+      end
+    end
+
+    def info(contents = '')
       pre_process(contents) do |content|
         super(content)
       end
     end
 
-    def warn(contents = "")
+    def warn(contents = '')
       pre_process(contents) do |content|
         super(content)
       end
     end
 
-    def debug(contents = "")
+    def debug(contents = '')
       pre_process(contents) do |content|
         super(content)
       end
     end
 
-    def error(contents = "")
+    def error(contents = '')
       pre_process(contents) do |content|
         super(content)
       end
     end
 
-    def fatal(contents = "")
+    def fatal(contents = '')
       pre_process(contents) do |content|
         super(content)
       end
